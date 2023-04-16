a6469c77584075b9935e9f969feff4eb02f58f9c
unknown <ahgusdl288@naver.com>
2023-04-16 19:39:40 +0900

    자유 게시판 전체 글 불러오기, 특정 글 불러오기 구현

--- src/main/java/com/woojujumin/controller/FreeBbsController.java
+++ src/main/java/com/woojujumin/controller/FreeBbsController.java
@@ -1,8 +1,13 @@
 package com.woojujumin.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.woojujumin.dto.FreeBbsDto;
 import com.woojujumin.service.FreeBbsService;
 
 @RestController
@@ -10,5 +15,15 @@ public class FreeBbsController {
 
 	@Autowired
 	FreeBbsService service;
+	
+	@GetMapping("getAllList")
+	public List<FreeBbsDto> getAllList() {
+		return service.getAllList();
+	}
+	
+	@GetMapping("getBbs")
+	public List<FreeBbsDto> getBbs(int bbsSeq) {
+		return service.getBbs(bbsSeq);
+	}
 	
 }
